6c97c06e792d433deb218e220329d6d634afd991
gjtjdwl <panggoon6@gmail.com>
2024-11-04 18:21:40 +0900

    feat: 로그인 기능을 위한 유저 정보 get, post, 게임정보 get, post 기능 추가

--- app.js
+++ app.js
@@ -1,12 +1,86 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+import asyncHandler from "./AsyncHandler.js";
 
+const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 
-const asyncHandler = 0;
-
-app.get("/", (req, res) => {
-  res.json("hello");
+app.get("/user", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.send(users);
 });
+
+app.get("/user/:userId", async (req, res) => {
+  const { userId } = req.params;
+  const users = await prisma.user.findUniqueOrThrow({
+    where: { userId },
+    include: { gamerecords: true },
+  });
+  res.send(users);
+});
+
+app.get("/:userId/gameRecord", async (req, res) => {
+  const { userId } = req.params;
+  const users = await prisma.gameRecord.findMany({
+    where: { userId },
+  });
+  res.send(users);
+});
+
+app.post(
+  "/user",
+  asyncHandler(async (req, res) => {
+    const user = await prisma.user.create({ data: req.body });
+    res.status(201).send(user);
+  })
+);
+
+app.post(
+  "/:userId/gameRecord",
+  asyncHandler(async (req, res) => {
+    const { userId } = req.params;
+    const { versus, win, kill, death } = req.body;
+    const user = await prisma.user.findUnique({
+      where: { userId },
+    });
+    const { nickname } = await prisma.user.findUnique({
+      where: { userId },
+      select: { nickname: true },
+    });
+    const data = {
+      userId,
+      versus,
+      win,
+      kill,
+      death,
+      nickname,
+    };
+    console.log(data);
+    if (user) {
+      const record = await prisma.gameRecord.create({
+        data: data,
+      });
+      res.status(201).send(record);
+    } else {
+      return res
+        .status(400)
+        .send("해당 userId를 가진 사용자가 존재하지 않습니다.");
+    }
+  })
+);
+
+app.delete(
+  "/user/:userId",
+  asyncHandler(async (req, res) => {
+    const { userId } = req.params;
+    const user = await prisma.user.delete({ where: { userId } });
+    if (user) {
+      res.sendStatus(204);
+    } else {
+      res.status(404).send({ message: "Not Found" });
+    }
+  })
+);
 
 app.listen(5000, () => console.log("Server Started"));

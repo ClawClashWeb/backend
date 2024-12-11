d981607df17c251b270d7b905bf80686a1a32238
gjtjdwl <panggoon6@gmail.com>
2024-12-11 22:51:19 +0900

    해쉬 값 받을 수 있도록 userpassword 유효성검사 수정

--- struct.js
+++ struct.js
@@ -2,6 +2,6 @@ import * as s from "superstruct";
 
 export const createUser = s.object({
     userId: s.size(s.string(), 2, 20),
-    userpassword: s.size(s.string(), 2, 20),
+    userpassword: s.string(),
     nickname: s.string(),
 });
